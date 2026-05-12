da9270f63ad6d4fd3dcd27055dac40ee9c3e812f
andreaballabio <groovedistrictservice@gmail.com>
2026-05-12 17:40:20 +0200

    debug: fix css and add test page

---
+++ apps/web/src/app/test/page.tsx
@@ -0,0 +1,8 @@
+export default function TestPage() {
+  return (
+    <div style={{ padding: 40, background: 'black', color: 'white', minHeight: '100vh' }}>
+      <h1>Test Page Works!</h1>
+      <p>If you see this, the deployment is working.</p>
+    </div>
+  )
+}
